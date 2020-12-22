db5b8c341f616910f2846ff1f966b76dcb7c012b
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2020-12-22 21:29:47 +0530

    string roman_to_int
    solution of LEETCODE(PRACTICE ) question of  string -> roman_to_int

---
+++ cpp/string/roman_to_int.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int romanToint(string s){
+    int i,res=0;
+        for(i=s.length()-1;i>=0;i--){
+            switch(s[i]){
+        case 'M':
+            res+=1000;
+            break;
+        case 'D':
+            res+=500;
+            break;
+        case 'C':
+            res+=100*(res>=500?-1:1);
+            break;
+        case 'L':
+            res+=50;
+            break;
+        case 'X':
+            res+=10*(res>=50?-1:1);
+            break;
+        case 'V':
+            res+=5;
+            break;
+        case 'I':
+            res+=1*(res>=5?-1:1);
+            break;
+        default:
+            break;
+            }
+        }
+        return res;
+ }
+
+int main(){
+string s;
+cin>>s;
+cout<<romanToint(s);
+}
