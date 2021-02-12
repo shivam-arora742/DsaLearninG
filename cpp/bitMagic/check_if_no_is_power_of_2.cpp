f7addae4598c04654a079043d7000b83d6b56bf2
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2021-02-12 20:17:05 +0530

    Update check_if_no_is_power_of_2.cpp

--- cpp/bitMagic/check_if_no_is_power_of_2.cpp
+++ cpp/bitMagic/check_if_no_is_power_of_2.cpp
@@ -19,3 +19,22 @@ else {
 }
 return 0;
 }
+////////////////////////////////////////////
+//Approach-2
+
+//User function Template for C++
+// Function to check if given number n is a power of two.
+
+bool isPowerofTwo(long long n){
+    
+    // Your code here
+    if(n==0){
+        return false;
+    }
+    if((n&n-1)==0){
+        return true;
+    }
+    return false;
+    
+}
+//execution time:0.16s
