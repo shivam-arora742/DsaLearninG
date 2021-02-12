c9fc46ca3a770339b9b02a930f0db1ef185b8bf3
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2021-02-12 20:33:21 +0530

    Add files via upload

---
+++ cpp/bitMagic/bit_difference.cpp
@@ -0,0 +1,15 @@
+//User function Template for C++
+
+// Function to find number of bits needed to be flipped to convert A to B
+int countBitsFlip(int a, int b){
+
+    // Your logic here
+    int res=(a^b);    //xor of a&b will give the different bits
+    int bits=0;       //to calculate set bit of result;
+    while(res>0){
+        res=(res&(res-1));    //count no of set bits in result
+        bits++;
+    }
+    return bits;
+}
+//execution time:0.18s
