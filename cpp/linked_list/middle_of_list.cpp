9fff88fc0127c3862def34ea33866dfb369e1da5
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2021-06-06 22:01:09 +0530

    Update middle_of_list.cpp

--- cpp/linked_list/middle_of_list.cpp
+++ cpp/linked_list/middle_of_list.cpp
@@ -1,4 +1,6 @@
-//Find Middle Point In List:Given a non-empty, singly linked list with head node head, return a middle node of linked list.If there are two middle nodes, return the second middle node.
+//LEETCODE-LINKED-LIST-PROBLEM
+//Find Middle Point In List:
+//Given a non-empty, singly linked list with head node head, return a middle node of linked list.If there are two middle nodes, return the second middle node.
 
 /**
  * Definition for singly-linked list.
