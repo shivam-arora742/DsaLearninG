e299da50aba08a9f55a128852f3b799dad5f588b
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2022-09-12 21:06:40 +0530

    Add files via upload

--- cpp/array/move_zeroes.cpp
+++ cpp/array/move_zeroes.cpp
@@ -1,12 +1,11 @@
-
 class Solution {
 public:
     void moveZeroes(vector<int>& nums){
-        int n=nums.size();          //number of elements in vector
-        for(int i=0;i<n;i++){      //traverse whole vector
-            if(nums[i]==0){        //find first '0'
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
                 int j=i+1;
-                while(j<n){           //traverse & replace '0' with other numeric data
+                while(j<n){
                     if(nums[j]!=0){
                         nums[i]=nums[j];
                         nums[j]=0;
@@ -18,5 +17,3 @@ public:
         }
     }
 };
-//runtime:16ms
-//memory:8.8mb
