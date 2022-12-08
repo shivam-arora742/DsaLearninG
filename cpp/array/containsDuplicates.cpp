c2a75143464d6dcb0e130b4d8535cd575ce72003
Shivam Arora <72338664+shivam-arora742@users.noreply.github.com>
2022-12-08 12:36:30 +0530

    Add files via upload

---
+++ cpp/array/containsDuplicates.cpp
@@ -0,0 +1,54 @@
+// Leetcode- Array-Easy: "Contains Duplicates":
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+// Code- Memory Effecient(80% less memory):
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// ---------------------------------------------------------------------
+
+// Code- Time Effecient (72% faster) :
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // create unordered set to store distinct values only:
+        unordered_set<int> s;
+
+        // iterate over nums &  insert distinct values in set:
+        for(auto itr : nums){
+            s.insert(itr);
+        }
+
+        // now compare sizes of both set & nums:
+        if(nums.size()==s.size()) return false;
+        else return true;
+    }
+};
